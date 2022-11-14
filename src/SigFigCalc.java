ddae34af11300e3c45cf5f48c5a51e4a8b01bd4b
BT_4N2_03 <ssakhawat4381@bths.edu>
2022-11-14 12:28:47 -0500

    Fixed and removed certain methods. Working on the countingSigFIgs method, which only needs you to count the number of characters that are significant. Possibly add a "precise comparer" method where you compare too numbers and see which has more sigfigs

--- src/SigFigCalc.java
+++ src/SigFigCalc.java
@@ -1,48 +1,41 @@
+import java.math.*;
 /** PRE-CONDITIONS
  * ALL real numbers are fair game (negatives, positives, rational numbers, etc.)
  * your number cannot be irrational (e.g. 2/3, pi, etc.)
  */
 public class SigFigCalc {
-    private double input; // sets input from user input of a number
+    private String input; // sets input from user input of a number
 
     // adds the number the user inputs as an attribute to the sigFigCalc object
-    public SigFigCalc(double number){
+    public SigFigCalc(String number){
         input = number;
     }
 
-    // Based on the number given, we will find out if the number has trailing zeros
-    /** CONDITIONS
-     * The first number has to be significant (over 0)
-     * every digit afterwards must be 0 */
-    public boolean trailingZeros(double number){
-        String str = "" + number;
-        for(int i = 0; i < str.length() - 1; i++){
-            if(str.charAt(1) == 0){
-                return false;
-            } else if()
+
+    // We can count how many significant figures are found within the input number
+    // This is kind of the "main" method that will determine the number of significant figures.
+    public int countSigFigs(String number){
+        String noDecNum = number.substring(0, number.indexOf(".")) + number.substring(number.indexOf(".") + 1);
+        int returnNum = 0;
+        if(number.indexOf(".") > -1){
+            boolean findNonZero = false;
+            int indexChecker = 0;
+            while(!findNonZero){
+                if((noDecNum.indexOf(indexChecker) > 0) && (indexChecker < noDecNum.length())){
+                    noDecNum = noDecNum.substring(noDecNum.indexOf(indexChecker));
+                    findNonZero = true;
+                }
+                indexChecker++;
+            }
+
+        } else{
+            // count sigfigs but reject the trailing zeros
         }
+        return 1; // just something random to help model this thing out
+    }
+    public void giveAnswer(){
 
     }
-
-    public boolean leadingZeros(double number){
-        String str = "" + number;
-        if(str.indexOf("1") != )
-    }
-    // Based on the number given, if a 0 exists in the number, and it is between significant numbers, it returns true
-    /** CONDITIONS
-     * The first and last numbers given must be significant
-     * If there are multiple zeros between significant numbers (e.g. 10001), it returns true
-     * if there are multiple separate zeros between significant numbers (e.g. 1010101), it returns true */
-    public boolean betweenSigNum(double number){
-
-    }
-
-    // Using the booleans above, we can count how many significant figures are found within the input number
-    // This is kind of the "main" method that will determine the number of significant figures, the other booleans are there as a helper class of sorts.
-    public int countSigFigs(){
-
-    }
-
     public String toString(){
         String rules = "To determine the number of significant figures in a number use the following three rules: \n" +
                 "1. Non-zero numbers are always significant.\n" +
