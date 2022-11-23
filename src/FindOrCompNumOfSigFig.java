9bbf8f26c37f18a5f3b30bef5eec7153280f2405
SikderSakhawat <114007137+SikderSakhawat@users.noreply.github.com>
2022-11-23 04:12:46 -0500

    Documented my methods and objects

--- src/FindOrCompNumOfSigFig.java
+++ src/FindOrCompNumOfSigFig.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
 public class FindOrCompNumOfSigFig {
     public static void main(String[] args) {
-        SigFigCalc n = new SigFigCalc("0.0000");
-        int food = n.countSigFigs();
-        System.out.println(food);
+        Scanner s = new Scanner(System.in);
+        System.out.println("----COMPARE OR FIND SIGNIFICANT FIGURES----");
+        System.out.println(s.toString());
+        System.out.println("Would you like to compare how precise two numbers are, or check the number of significant figures of a number?\n" +
+                "(1 = comparing two values, 2 = counting the number of significant figures of a number)");
+        String response = s.nextLine();
+        if (response.equals("1")) {
+            System.out.println("What is your first number?");
+            String firstNum = s.nextLine();
+            System.out.println("What is your second number?");
+            String secondNum = s.nextLine();
+            SigFigCalc compareNums = new SigFigCalc(firstNum, secondNum);
+            compareNums.morePreciseNum(firstNum, secondNum);
+        } else if (response.equals("2")) {
+            System.out.println("Enter the number that you want to find the significant figures of:");
+            String answer = s.nextLine();
+            SigFigCalc checkNum = new SigFigCalc(answer);
+            int numSigFigs = checkNum.countSigFigs(answer);
+            System.out.println("Your number " + answer + " has " + numSigFigs + " significant figures. That is pretty precise!");
+        } else {
+            System.out.println("Please enter the correct number next time you use this. Goodbye.");
+        }
+        int randNum = (int) (Math.random() * 10) + 1;
+        System.out.println("I'm not done with you yet! Can you give me a number with " + randNum + " significant figure(s)?");
+        String guessAns = s.nextLine();
+        SigFigCalc guessNum = new SigFigCalc(guessAns);
+        if (guessNum.equalToNumSigFig(randNum, guessAns))
+            System.out.println("Wow, you truly are a significant numbers master!");
+        else {
+            System.out.println("No you didn't get it right! :(");
+        }
     }
 }
-
--- src/SigFigCalc.java
+++ src/SigFigCalc.java
@@ -1,40 +1,55 @@
+import java.util.Objects;
 /** PRE-CONDITIONS
  * ALL real numbers are fair game (negatives, positives, rational numbers, etc.)
  * your number cannot be irrational (e.g. 2/3, pi, etc.)
+ * ALL numbers cannot exceed the max number of integers or doubles; it will produce only the max number of significant figures in that case
  */
 public class SigFigCalc {
     private String inputNum; // sets input from user input of a number
     // adds the number the user inputs as an attribute to the sigFigCalc object
+
+    /**
+     * used to instantiate the input num attribute to my object
+     * @param number represents the number that the user chooses to find the number of significant figures for that number.
+     */
     public SigFigCalc(String number){
         this.inputNum = number;
     }
+    /**
+     * An object used to model two numbers that will be compared to each other to figure out which number is more precise based on the number
+     of significant figures it has
+     * @param num1 represents one number that will be compared
+     * @param num2 represents the other number that is being compared to num1
+     */
     public SigFigCalc(String num1, String num2){}
-
-    // We can count how many significant figures are found within the input number
-    // This is kind of the "main" method that will determine the number of significant figures.
-    public int countSigFigs(){
-        int numAsInt = 0;
-        double numAsDouble = 0;
+    /**
+     * Essentially just counts the number of significant figures a given number has, regardless of whther it is negative, positive, or rational.
+     * Only limitations it has is when it exceeds Java's max integer/double values.
+     * @param inputNumber this depicts the number that the user inputs in  order to figure out the number of significant figures it contains.
+     * @return returns an integer with the number of significant figures. Cannot be a negative number.
+     */
+    public int countSigFigs(String inputNumber){
+        int numAsInt;
+        double numAsDouble;
         String numStr = "";
-        String noDecNum = "";
+        String noDecNum;
         // variables above initialize what is needed to have the rest of the method to run
-        if(Double.parseDouble(inputNum) % 1 == 0) {
-            numAsInt = (int) Math.abs(Double.parseDouble(inputNum));
-            numStr += numAsInt;
+        if(Double.parseDouble(inputNumber) % 1 == 0) {
+            numAsInt = (int) Math.abs(Double.parseDouble(inputNumber));
             noDecNum = "" + numAsInt;
         } else {
-            numAsDouble = Math.abs(Double.parseDouble(inputNum));
+            numAsDouble = Math.abs(Double.parseDouble(inputNumber));
             numStr+= numAsDouble;
             int whereDecPoint = numStr.indexOf(".");
             noDecNum = numStr.substring(0, whereDecPoint) + numStr.substring(whereDecPoint + 1);
         }
-        if(Double.parseDouble(inputNum) == 0){
+        if(Double.parseDouble(inputNumber) == 0){
             return 0;
-        } else if (inputNum.contains(".")){
+        } else if (inputNumber.contains(".")){
             int firstSigNum = 0;
             int indexChecker = 0;
             boolean findNonZero = false;
-            if(inputNum.contains(".")){
+            if(inputNumber.contains(".")){
                 while(!findNonZero && indexChecker <= noDecNum.length()){
                     if(Integer.parseInt(noDecNum.substring(indexChecker, indexChecker + 1)) > 0){
                         firstSigNum = indexChecker;
@@ -67,25 +82,45 @@ public class SigFigCalc {
         return noDecNum.length();
     }
 
-    public boolean containsDecimal(String num) {
-        String indexChecker = "";
-        for (int i = 0; i <= num.length() - 1; i++) {
-            indexChecker = "" + num.charAt(i);
-            if(indexChecker == "."){
-                return true;
-            }
+    /**
+     * used to compare two numbers and see which has more significant numbers. Print statement depends on which is more "precise".
+     * @param num1 one of the numbers being compared to
+     * @param num2 the other number being compared to
+     */
+    public void morePreciseNum(String num1, String num2){
+        int numSigFig1 = countSigFigs(num1);
+        int numSigFig2 = countSigFigs(num2);
+        if(numSigFig1 > numSigFig2){
+            System.out.println(num1 + " is more precise than " + num2 + "!");
+        } else if(numSigFig1 < numSigFig2){
+            System.out.println(num2 + " is more precise than " + num1 + "!");
+        } else {
+            System.out.println("Both numbers are equally as precise!");
+        }
+    }
+
+    /**
+     * Determines if the inputNum has the same number of significant figures as stated in the numSigFig parameter
+     * @param numSigFig determines how many significant figures a number should be rounded to
+     * @param inputNum determines the number that will be checked
+     * @return returns true if inputNum has numSigFig significant figures. Returns false otherwise.
+     */
+     public boolean equalToNumSigFig(int numSigFig, String inputNum){
+        if(countSigFigs(inputNum) == numSigFig){
+            return true;
         }
         return false;
-    }
+     }
 
-    public void giveAnswer(){
-
-    }
+    /**
+     * Explains the rules of significant figures to the user.
+     * @return Returns the rules of significant figures to the player, if they need a refresher for what a significant figure is (Mainly Mr. Das)
+     */
     public String toString(){
-        String rules = "To determine the number of significant figures in a number use the following three rules: \n" +
-                "1. Non-zero numbers are always significant.\n" +
-                "2. Any zeros between two significant digits are significant\n" +
-                "3. A final zero or trailing zeros are ONLY significant in decimals.";
-        return rules;
+        return """
+                To determine the number of significant figures in a number use the following three rules:\s
+                1. Non-zero numbers are always significant.
+                2. Any zeros between two significant digits are significant
+                3. A final zero or trailing zeros are ONLY significant in decimals.""";
     }
 }
